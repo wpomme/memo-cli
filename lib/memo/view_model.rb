349e768b400fa1b4f0575019a96644375213e0bf
yuya hashimoto <ne25qc3b@gmail.com>
2026-07-19 00:28:46 +0900

    Add file_list_to_presenter to use memo list <dir>

--- lib/memo/view_model.rb
+++ lib/memo/view_model.rb
@@ -4,7 +4,26 @@ require 'rainbow'
 
 module Memo
   class ViewModel
-    def self.file_list_to_presenter(memo_dir)
+    def self.file_list_to_presenter(memo_dir, dir = nil)
+      ## REVIEW: コードの書きっぷりが情けないほどダブってるので描き直したい
+      if dir
+        ret = Memo::Repository.new(memo_dir).grouped_file_list.filter_map do |struct|
+          [Rainbow(struct[:dir]).green].append(struct[:filenames], "\n") if struct[:dir] == dir
+        end.flatten
+        ret.pop
+
+        ## dir が存在する場合
+        return ret unless ret.empty?
+
+        ## dir が存在しない場合は、ユーザーに表示するメッセージを返す
+        colored_dirs = Memo::Repository.new(memo_dir).to_dirs.map { |dir| Rainbow(dir).green }
+        return <<~NOT_DIR
+          #{dir}というディレクトリはありませんでした。
+          ディレクトリの一覧は次の通りです。
+          #{colored_dirs.join(' ')}
+        NOT_DIR
+      end
+
       ret = Memo::Repository.new(memo_dir).grouped_file_list.map do |struct|
         [Rainbow(struct[:dir]).green].append(struct[:filenames], "\n")
       end.flatten
--- test/memo/test_view_model.rb
+++ test/memo/test_view_model.rb
@@ -7,7 +7,8 @@ class TestViewModel < Minitest::Test
     include MemoTestLifecycleHooks
 
     describe '#file_list_to_presenter' do
-      it "Repositoryから受け取ったグループ化されたファイル名の一覧をPresenterで表示しやすくする" do
+      # ディレクトリを緑色にするなど
+      it "グループ化されたファイル名の一覧をPresenterで表示しやすくする" do
         expected = Memo::ViewModel.file_list_to_presenter(@memo_dir)
 
         # entries.group_by(&:dir)はMemoFileUtility.entries_grouped_by_dirと同じ
@@ -26,6 +27,41 @@ class TestViewModel < Minitest::Test
         actual.pop
 
         _(actual).must_equal(expected)
+      end
+
+      it "有効なディレクトリ名を受け取った場合は、そのディレクトリとその中のファイル名を表示する" do
+        valid_dir = 'cli'
+        expected = Memo::ViewModel.file_list_to_presenter(@memo_dir, valid_dir)
+
+        grouped_file_list = @test_repository_entries.group_by(&:dir).filter_map do |dir, entry|
+          if dir == valid_dir
+            Memo::GroupedFileList.new(
+              dir: dir,
+              filenames: entry.map(&:filename).sort
+            )
+          end
+        end
+
+        actual = grouped_file_list
+          .map do |struct|
+            [Rainbow(struct[:dir]).green].append(struct[:filenames], "\n")
+          end.flatten
+
+        actual.pop
+
+        _(actual).must_equal(expected)
+      end
+
+
+      it "存在しないディレクトリ名を受け取った場合は、その旨を知らせる文字列を返す" do
+        invalid_dir = 'invalid_dir'
+        expected = Memo::ViewModel.file_list_to_presenter(@memo_dir, invalid_dir)
+
+        pp "デバッグ"
+        pp expected
+
+        # TODO: とりあえず文字列を返すことだけを確認する
+        assert expected.is_a?(String)
       end
     end
   end
